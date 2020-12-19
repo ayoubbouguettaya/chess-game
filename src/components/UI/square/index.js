02f3a8e16a778a232a575423a220284ad68ceca4
bouguettaya ayoub <ayoubbouguettaya1616@gmail.com>
2020-12-19 19:55:50 +0100

    add logic select piece  to square componenet

--- src/components/UI/square/index.js
+++ src/components/UI/square/index.js
@@ -1,22 +1,32 @@
-import React,{useState} from 'react';
+import React, { useContext, useState } from 'react';
 
 import styles from './square.module.css';
+import { gameContext } from '../../../store';
+import { SELECT_PIECE } from '../../../store/action';
+import { checkIsSelected } from '../../../store/function';
 
 const Square = ({ color, piece, player, row, column }) => {
-    const  [isChecked,setIsChecked] = useState(false);    
-    
-    const imageSrc = `/assets/${piece}${player === 2 ? '_' : ''}.png`;
-    
+    const imageSrc = `/assets/${piece}${player === 1 ? '_' : ''}.png`;
+
+    const { dispatch, gameState: { player: myPlayer, selectedSquare } } = useContext(gameContext);
+    const isSelected = checkIsSelected({ row, column }, selectedSquare)
+
     const handleSelect = () => {
-        setIsChecked((state) => !state);
+        if (selectedSquare) {
+            console.log('move action require :1/ the global state,2/ selected square, 3/ square to move');
+        }
+
+        if (!isSelected && piece !== 'empty' && myPlayer === player) {
+            return dispatch({ type: SELECT_PIECE, payload: { row, column } })
+        }
     }
-    
+
     return (
-        <div 
-        className={`${styles.square} ${isChecked ? styles.is_checked : ''} ${color === 'black' ? styles.square_black : styles.square_white}`}
-        onClick={handleSelect}
+        <div
+            className={`${styles.square} ${isSelected ? styles.is_checked : ''} ${color === 'black' ? styles.square_black : styles.square_white}`}
+            onClick={handleSelect}
         >
-            {(piece !== 'empty') ? <img src={imageSrc}/> : '.'} 
+            {(piece !== 'empty') ? <img src={imageSrc} /> : '.'}
         </div>
     );
 };
