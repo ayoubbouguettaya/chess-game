6c77bb3f610dc4612b2179d60268dc2a3d2cb46a
bouguettaya ayoub <ayoubbouguettaya1616@gmail.com>
2020-12-19 11:38:59 +0100

    fix eslint warning

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,12 +2,9 @@ import intialiseGame from './function';
 import { INITIALIZE_GAME } from './action';
 
 export const gameReducer = (state, action) => {
-
     switch (action.type) {
         case INITIALIZE_GAME:
             return intialiseGame();
-            break;
-
         default:
             break;
     }
