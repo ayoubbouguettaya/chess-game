cedd03801f500acbdf2e4a79978ee44618bbc0dc
bouguettaya ayoub <ayoubbouguettaya1616@gmail.com>
2020-12-25 15:05:57 +0100

    add div to dispaly allowed movement and myPlayer

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 
 import styles from './home.module.css';
 import Board from '../UI/board';
+import { gameContext } from '../../store';
 
 const HomeComponent = () => {
+    const { gameState: { myPlayer, allowedSquare } } = useContext(gameContext);
+
     return (
         <div className={styles.home_container} >
             <div className={styles.board_container}>
                 <Board />
+            </div>
+            <div>
+                <h2>Player: {myPlayer}</h2>
+                <h2 style={{fontWeight: '300'}}>Allowed movement</h2>
+                {allowedSquare && allowedSquare.map(((square) => <p>{`${square.row} : ${square.column}`}</p>))}
             </div>
         </div>
     )
