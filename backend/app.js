2279550b907909200cc1f92a9b0090bd5ee1aad0
bouguettaya ayoub <ayoubbouguettaya1616@gmail.com>
2021-01-05 14:47:24 +0100

    backend logic snipet code (draft)

--- backend/app.js
+++ backend/app.js
@@ -10,23 +10,55 @@ const indexRouter = require('./routes/index');
 
 const app = express();
 
-let NbrConnectedUser = 0;
+let connectedUser = [];
+let gameNumber = 0;
+
+const removeUser = (array, value) => {
+  let index = 0;
+  while (index < array.length) {
+    if (array[index] === value) {
+      array.splice(index, 1);
+    } else {
+      index++;
+    }
+  }
+  return array;
+};
+
 io.on("connection", (socket) => {
-  console.log(socket.id);
-  console.log(`NbrConnectedUser: ${NbrConnectedUser}`);
-  NbrConnectedUser++;
 
   socket.on('disconnect', (reason) => {
-    NbrConnectedUser--;
-    console.log(reason);
+    const index = connectedUser.indexOf(socket.id);
+    connectedUser = removeUser(connectedUser, socket.id);
+    if (index % 2 === 0) {
+      connectedUser = removeUser(connectedUser, connectedUser[index]);
+    } else {
+      connectedUser = removeUser(connectedUser, connectedUser[index - 1]);
+    }
+  });
+
+  socket.on('initialize_game', (data) => {
+    const myPlayer = (connectedUser.length % 2) + 1;
+    if (connectedUser.length % 2 === 0) {
+      gameNumber++;
+    }
+    connectedUser.push(socket.id);
+
+    console.log('gameNumber:', gameNumber);
+    console.log('myPlayer:', myPlayer);
+    console.log('number of connected user', connectedUser.length)
+    console.log('******--------------------****************');
+
+    socket.emit('initialize_game', { gameNumber, myPlayer });
   });
 
   socket.on('move', (data) => {
-
-  });
-
-  socket.on('initialize-game', (data) => {
-
+    const { selectedSquare, nextSquare } = data;
+    const index = connectedUser.indexOf(socket.id)
+    const opponentId = (index % 2 === 0) ? connectedUser[index + 1] : connectedUser[index - 1];
+    io.to(opponentId).emit('move', {
+      IncomingSelectedSquare: selectedSquare, IncommingNextSquare: nextSquare,
+    });
   });
 });
 
